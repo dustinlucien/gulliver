c029e612b0fdb54077b956f7e25dccd1b938dffe
Dustin Lucien <dustin.lucien@gmail.com>
2011-02-25 17:49:01 -0500

    initializing rails project for passport photo and travel deals site.  woohooo!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gulliver::Application.config.secret_token = '0a73c37ee152d0f104cfdb3c3bc3b16568933a552559bbecf380da4b16dfce73e1182e4714308be7dfbf6027ab13aaf59f5fa3c278068df79832e6c65dde3c05'
