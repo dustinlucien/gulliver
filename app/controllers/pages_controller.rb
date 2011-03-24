1c12ac2dc51309c8cd411e568d0bca6cb4f7ea90
Dustin Lucien <dustin.lucien@gmail.com>
2011-03-24 07:44:43 -0400

    better titling of pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,21 @@
 class PagesController < ApplicationController
   def home
-    @title = "Get your passport photos online - Passport Photos, Visa Photos, and ID Photos"
+    @title = "Get your passport photos online"
   end
 
   def about
-    @title = "Get your passport photos online - Passport Photos, Visa Photos, and ID Photos - Why use our service?"
+    @title = "About Us | Why use our service?"
   end
 
   def contact
-    @title = "Get your passport photos online - Passport Photos, Visa Photos, and ID Photos - How can we help?"
+    @title = "Contact Us | How can we help?"
   end
 
   def howto
-    @title = "Get your passport photos online - Passport Photos, Visa Photos, and ID Photos - Instructions on taking the perfect passport photo"
+    @title = "How to take perfect passport photo"
   end
 
   def privacy
-    @title = "Get your passport photos online - Passport Photos, Visa Photos, and ID Photos - Privacy Policy"
+    @title = "Privacy Policy | We take your privacy seriously"
   end
 end
